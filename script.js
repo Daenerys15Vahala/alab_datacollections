6458a48a903b1c00f2779e90058866dbfdda106e
Valentina BM <valentina15bermo@gmail.com>
2026-07-12 22:34:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// DATA COLLECTIONS LAB
+
+// PART 1 //
+
+const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+
+const rows = csv.split("\n");
+console.log(rows);
